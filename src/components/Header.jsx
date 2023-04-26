e2147d8af8d89cfc276fb79ce9002965438e9268
SekoViper <damehlivingstone1@gmail.com>
2023-04-26 14:27:17 +0000

    Add login component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,7 @@ const Header = () => (
         <ul>
           <li><NavLink to="/">Home</NavLink></li>
           <li><NavLink to="/signup">Sign up</NavLink></li>
+          <li><NavLink to="/login">Log in</NavLink></li>
         </ul>
       </div>
     </div>
