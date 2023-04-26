40a68b457be4fd2c032f186fa0f14544940514c1
SekoViper <damehlivingstone1@gmail.com>
2023-04-26 14:26:02 +0000

    Add routing.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
 } from 'react-router-dom';
 import Signup from './components/Signup/Signup';
 import Header from './components/Header';
+import Login from './components/Login/Login';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Header />
         <Routes>
           <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
