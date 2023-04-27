7cb3627c7ddf7b3cfe9ba69c150285823ab7f5ef
AbdoNJ <mr.nejjari.abdelhafid@gmail.com>
2023-04-27 11:01:22 +0100

    Add Addcars components

--- src/components/Nav/Header.jsx
+++ src/components/Nav/Header.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './Header.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => (
   <div className="header">
     <h1>
-      <NavLink to="/" className="logo">Tesla Cars Rental</NavLink>
+      <NavLink to="/" className="logo">
+        Tesla Cars Rental
+      </NavLink>
     </h1>
     <nav>
       <ul>
         <li>
-          <NavLink to="/" activeClassName="active">Home</NavLink>
+          <NavLink to="/" activeClassName="active">
+            Home
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/signup" activeClassName="active">Sign up</NavLink>
+          <NavLink to="/signup" activeClassName="active">
+            Sign up
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/login" activeClassName="active">Log in</NavLink>
+          <NavLink to="/login" activeClassName="active">
+            Log in
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/addcars" activeClassName="active">
+            Add Cars
+          </NavLink>
         </li>
       </ul>
     </nav>
