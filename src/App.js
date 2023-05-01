1f1dbaaa9aa994b137104412d582311ec4fb32cc
AhmadiGu <ahmadigulalai76@gmail.com>
2023-05-01 14:54:07 +0500

    Main page and Navbar component and design created

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ReservedCars from './components/Reservation/ReservedCars';
 import CarDetails from './components/Car/Detials/CarsDetails';
 import Cars from './components/Car/Cars';
 import AddCar from './components/Car/AddCars';
+import Home from './components/Home/Home';
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
         <div className="menu fixed left-0 top-0 bottom-0 h-screen w-52 shadow-lg py-10 px-4 fade-in col-2"><Header /></div>
         <div className="content col-10">
           <Routes>
-            <Route exact path="/" element={<Cars />} />
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/cars" element={<Cars />} />
             <Route exact path="/signup" element={<Signup />} />
             <Route exact path="/login" element={<Login />} />
             <Route exact path="/new_reservations" element={<ReservationNew />} />
