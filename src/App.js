160e1bacd0b0647f2e8461f34f503895877907bf
AhmadiGu <ahmadigulalai76@gmail.com>
2023-05-05 13:31:12 +0500

    App  modified

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Signup from './components/Signup/Signup';
 import Header from './components/Nav/Header';
@@ -10,22 +11,32 @@ import AddCar from './components/Car/AddCars';
 import Home from './components/Home/Home';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <Router>
       <div className="App row">
-        <div className="menu fixed left-0 top-0 bottom-0 h-screen w-52 shadow-lg py-10 px-4 fade-in col-2"><Header /></div>
-        <div className="content col-10">
+        {isLoggedIn ? (
+          <>
+            <div className="menu fixed left-0 top-0 bottom-0 h-screen w-52 shadow-lg py-10 px-4 fade-in col-2">
+              <Header />
+            </div>
+            <div className="content col-10">
+              <Routes>
+                <Route exact path="/cars" element={<Cars />} />
+                <Route exact path="/new_reservations" element={<ReservationNew />} />
+                <Route exact path="/reservedCars" element={<ReservedCars />} />
+                <Route exact path="/addcars" element={<AddCar />} />
+                <Route exact path="/cars/:id" element={<CarDetails />} />
+              </Routes>
+            </div>
+          </>
+        ) : (
           <Routes>
             <Route exact path="/" element={<Home />} />
-            <Route exact path="/cars" element={<Cars />} />
             <Route exact path="/signup" element={<Signup />} />
             <Route exact path="/login" element={<Login />} />
-            <Route exact path="/new_reservations" element={<ReservationNew />} />
-            <Route exact path="/reservedCars" element={<ReservedCars />} />
-            <Route exact path="/addcars" element={<AddCar />} />
-            <Route exact path="cars/:id" element={<CarDetails />} />
           </Routes>
-        </div>
+        )}
       </div>
     </Router>
   );
